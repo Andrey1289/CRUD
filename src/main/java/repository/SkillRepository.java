c5d8143610b2b5c03e7d1c35c5f97dfb21d912d5
Andrey Galkovskii <dobpuha@gmail.com>
2021-08-29 14:32:30 +0700

    check please

--- src/main/java/repository/SkillRepository.java
+++ src/main/java/repository/SkillRepository.java
@@ -1,55 +1,137 @@
 package repository;
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonArray;
 import com.google.gson.reflect.TypeToken;
 import model.Skill;
 
 import java.io.*;
 import java.lang.reflect.Type;
-import java.nio.file.Files;
-import java.nio.file.InvalidPathException;
-import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
 public class SkillRepository {
-    private static final Gson gson = new Gson();
-    private static final String FILE_PATH ="C:\\Users\\dobpu\\IdeaProjects\\CRUDAplications\\src\\main\\resources\\skills.json";
+    private  final Gson gson = new Gson();
+    private  final String FILE_PATH ="C:\\Users\\dobpu\\IdeaProjects\\CRUDAplications\\src\\main\\resources\\skills.json";
 
-    public static void main(String[] args) {
+       Skill getById(Long id){
+        Skill ById = null;
+        String json =null;
+        try (BufferedReader bf = new BufferedReader(new FileReader(FILE_PATH));){
+            json = bf.readLine();
 
-        getAll();
+        }catch (NullPointerException e){
+            e.printStackTrace();
+        } catch (IOException e){
+        e.printStackTrace();
     }
-    Skill getById(Long id){
-        return null;
-    }
- static List<Skill> getAll() {
+        Type targetClassType = new TypeToken<ArrayList<Skill>>(){}.getType();
+        ArrayList<Skill> skills = gson.fromJson(json,targetClassType);
+        for (Skill skill : skills) {
+            if (skill.getId().equals(id)){
+             ById= skill;
+            }
+        }
+return ById;
+
+   }
+  List<Skill> getAll() {
         String json =null;
       try (BufferedReader bf = new BufferedReader(new FileReader(FILE_PATH));){
             json = bf.readLine();
-
 
       }catch (IOException e){
         e.printStackTrace();
       }
      Type targetClassType = new TypeToken<ArrayList<Skill>>(){}.getType();
     ArrayList<Skill> skills = gson.fromJson(json,targetClassType);
-     System.out.println(skills);
        return skills;
-
-
-
     }
     Skill save(Skill skill){
+        String json =null;
+        try (BufferedReader bf = new BufferedReader(new FileReader(FILE_PATH));){
+            json = bf.readLine();
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        Type targetClassType = new TypeToken<ArrayList<Skill>>(){}.getType();
+        ArrayList<Skill> skills = gson.fromJson(json,targetClassType);
+          skills.add(skill);
+        String jsonCollection = gson.toJson(skills);
+        char[] buff = new char[jsonCollection.length()];
+       jsonCollection.getChars(0,jsonCollection.length(),buff,0);
+       try( FileWriter fw = new FileWriter(FILE_PATH);) {
+           for (int i = 0; i < buff.length; i++) {
+               fw.write(buff[i]);
+           }
+       }catch (IOException e){
+           e.printStackTrace();
+       }
+
         return null;
     }
     Skill update(Skill skill){
+        String json =null;
+        try (BufferedReader bf = new BufferedReader(new FileReader(FILE_PATH));){
+            json = bf.readLine();
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        Type targetClassType = new TypeToken<ArrayList<Skill>>(){}.getType();
+        ArrayList<Skill> skills = gson.fromJson(json,targetClassType);
+        ListIterator<Skill> litr = skills.listIterator();
+        while(litr.hasNext()){
+            Skill element = litr.next();
+            if(skill.getId().equals(element.getId()))
+                litr.set(skill);
+            if (skill.getName().equals(element.getName()))
+                litr.set(skill);
+        }
+
+       String jsonCollection = gson.toJson(skills);
+       char[] buff = new char[jsonCollection.length()];
+       jsonCollection.getChars(0,jsonCollection.length(),buff,0);
+       try( FileWriter fw = new FileWriter(FILE_PATH);) {
+           for (int i = 0; i < buff.length; i++) {
+               fw.write(buff[i]);
+           }
+       }catch (IOException e){
+           e.printStackTrace();
+       }
         return null;
     }
     void deleteById(Long id){
+        String json =null;
+        try (BufferedReader bf = new BufferedReader(new FileReader(FILE_PATH));){
+            json = bf.readLine();
+
+        }catch (NullPointerException e){
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        Type targetClassType = new TypeToken<ArrayList<Skill>>(){}.getType();
+        ArrayList<Skill> skills = gson.fromJson(json,targetClassType);
+        ListIterator<Skill> litr = skills.listIterator();
+        while(litr.hasNext()){
+            Skill element = litr.next();
+            if (element.getId().equals(id)){
+                litr.remove();
+            }
+        }
+       String jsonCollection = gson.toJson(skills);
+       char[] buff = new char[jsonCollection.length()];
+       jsonCollection.getChars(0,jsonCollection.length(),buff,0);
+       try( FileWriter fw = new FileWriter(FILE_PATH);) {
+           for (int i = 0; i < buff.length; i++) {
+               fw.write(buff[i]);
+           }
+       }catch (IOException e){
+           e.printStackTrace();
+       }
 
     }
 }
