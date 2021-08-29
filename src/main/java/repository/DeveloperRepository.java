497dc731a10fb61d916f08e522b196539644f5c3
Andrey Galkovskii <dobpuha@gmail.com>
2021-08-29 18:10:18 +0700

    Adding class Developer and DeveloperRepository class

---
+++ src/main/java/repository/DeveloperRepository.java
@@ -0,0 +1,23 @@
+package repository;
+
+import model.Developer;
+
+import java.util.List;
+
+public class DeveloperRepository {
+    Developer getById(Long id){
+        return null;
+    }
+    List<Developer> getAll(){
+        return null;
+    }
+    Developer save(Developer developer){
+     return developer;
+    }
+    Developer update(Developer developer ){
+        return developer;
+    }
+    void deleteById(Long id){
+
+    }
+}
